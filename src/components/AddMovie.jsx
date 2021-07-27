157ba9cddcf6b3fdc24ac9c39af6c9326b489dc8
mariananogueirab <mariananbp@gmail.com>
2021-07-27 15:54:34 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,14 @@
-// implement AddMovie component her
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form>
+        
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
