0856fddf951351f7269499b5efcaac893e43955e
mariananogueirab <mariananbp@gmail.com>
2021-07-27 19:17:28 -0300

    Requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,16 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie onClick={ () => console.log('teste1') } />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
