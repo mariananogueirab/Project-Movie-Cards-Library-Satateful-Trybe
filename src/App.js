07803e7200338cdd281cf40dcbee97a2024d7ec1
mariananogueirab <mariananbp@gmail.com>
2021-07-27 15:08:10 -0300

    Requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import SearchBar from './components/SearchBar'
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
